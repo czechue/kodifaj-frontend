860a096b22e2585a061c1c0a6546d86208114622
Michal Lester <michal.lester@allegro.pl>
2020-04-05 17:24:34 +0200

    add eslint, prettier, husky, lint-staged

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import "../styles/index.css";
+import React from 'react';
+import { AppProps } from 'next/app';
+import '../styles/index.css';
 
-import { AppProps } from 'next/app'
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
-}
-
-export default MyApp
+export default MyApp;
