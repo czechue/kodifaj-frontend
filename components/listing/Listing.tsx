7608b31f1db9ddf0ad379ffd8df5727d9396ca6b
jundymek <jundymek@gmail.com>
2020-04-18 16:52:18 +0200

    refactor: move .indent class to listingCard.css

--- components/listing/Listing.tsx
+++ components/listing/Listing.tsx
@@ -38,14 +38,24 @@ const Listing: React.FC<ListingProps> = ({ tasks = [] }) => {
                   style={{ backgroundImage: `url(${task.images[0]}` }}
                 ></div>
                 <div className="h-32 border-b-2 border-gray-200 relative">
-                  <div className="absolute left-0 top-0 indent z-10"></div>
-                  <h4 className="text-center pt-4 ml-2 text-lg">{task.title}</h4>
+                  <div className="absolute left-0 top-0 indent z-10">
+                    <span
+                      className="block ml-2 -mt-3 h-8 w-8 rounded-full z-50 relative bg-cover"
+                      style={{
+                        backgroundImage: `url(https://avatars2.githubusercontent.com/u/21274642?s=400&u=d2f9f822cccbbd841e2c37e2c9b790644f51b8d8&v=4`,
+                      }}
+                    ></span>
+                  </div>
+                  <h4 className="text-center pt-8 ml-2 text-lg">{task.title}</h4>
                 </div>
                 <div className="rounded-b-md">
                   <ul className="list-none px-1 py-2 text-right">
                     {task.tags.map((tag, index) => (
-                      <li className="inline px-1 bg-red-300 mr-1 rounded-md shadow-md" key={index}>
-                        {tag}
+                      <li
+                        className="inline px-1 py-1 bg-red-200 mr-1 rounded-md shadow-sm w-4"
+                        key={index}
+                      >
+                        <span className="text-sm">{tag}</span>
                       </li>
                     ))}
                   </ul>
