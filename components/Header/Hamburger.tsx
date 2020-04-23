732ff55c0ae95851aceceaa8e8207b1b2697d3f7
jundymek <jundymek@gmail.com>
2020-04-23 22:26:35 +0200

    refactor: custom classes for Hamburger - hamburger.css

--- components/Header/Hamburger.tsx
+++ components/Header/Hamburger.tsx
@@ -9,17 +9,11 @@ const Hamburger: React.FC<HamburgerProps> = ({ isOpen, toogle }) => {
   return (
     <button className={`md:hidden z-10 ${isOpen && 'fixed right-0 mr-4'}`} onClick={toogle}>
       <div
-        className={`w-6 h-2 border-t-4 border-gray-800 transition-transform duration-500 ${
-          isOpen && 'transform rotate-45 -translate-x-00 translate-y-2'
-        }`}
+        className={`hamburger-bar ${isOpen && 'transform rotate-45 -translate-x-00 translate-y-2'}`}
       ></div>
+      <div className={`hamburger-bar-middle  ${isOpen && 'transform opacity-0'}`}></div>
       <div
-        className={`w-6 h-2 border-t-4 border-gray-800 transition-all duration-500  ${
-          isOpen && 'transform opacity-0'
-        }`}
-      ></div>
-      <div
-        className={`w-6 h-2 border-t-4 border-gray-800 transition-transform duration-500 ${
+        className={`hamburger-bar ${
           isOpen && 'transform -rotate-45 translate-x-00 -translate-y-2'
         }`}
       ></div>
