af1626dac221c5446e4df070c51a6b6048e75805
jundymek <jundymek@gmail.com>
2020-04-23 23:39:26 +0200

    style: center text in Badges

--- ui/Badges/Badges.tsx
+++ ui/Badges/Badges.tsx
@@ -8,7 +8,10 @@ const Badges: React.FC<BadgesProps> = ({ tags }) => {
   return (
     <ul className="list-none py-1 text-right">
       {tags.map((tag, index) => (
-        <li className="inline px-1 bg-red-200 mr-1 mt-4 rounded-md shadow-sm w-4" key={index}>
+        <li
+          className="inline-flex w-auto px-1 bg-red-200 mr-1 mt-4 rounded-md shadow-sm w-4 items-center"
+          key={index}
+        >
           <span data-testid="tag" className="text-xs">
             #{tag}
           </span>
