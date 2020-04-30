7a40c5429c35e9040a83c0540136983c17d1a495
Michal Lester <michal.lester@allegro.pl>
2020-05-01 00:15:48 +0200

    add preview envs

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CALLBACK_URL: process.env.GITHUB_CALLBACK_URL,
     MONGO_URI: process.env.MONGO_URI,
-    WEB_URI: process.env.WEB_URI,
+    VERCEL_URL: process.env.VERCEL_URL,
     COOKIE_KEY: process.env.COOKIE_KEY,
   },
 };
