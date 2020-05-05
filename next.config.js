1e60dde26e29b7290d3fd47cb4cd541d32a1d007
jundymek <jundymek@gmail.com>
2020-05-05 20:21:37 +0200

    fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ require('dotenv').config();
 // });
 
 module.exports = {
+  distDir: '_next',
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  },
   env: {
     VERCEL_URL: process.env.VERCEL_URL,
     MONGO_URI: process.env.MONGO_URI,
