480eb1c2e75ae43102a0cbc1eefbc3d4a71a5249
jundymek <jundymek@gmail.com>
2020-05-05 23:25:57 +0200

    fix: Card Link taskid v3

--- components/listing/card/Card.tsx
+++ components/listing/card/Card.tsx
@@ -11,7 +11,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ task }) => {
   return (
     <div className="flex flex-col items-center">
-      <Link href="/tasks/[taskid]" as={`/tasks/${task._id.toString()}`}>
+      <Link href="/tasks/[taskid]" as={`/tasks/${task._id}`}>
         <a className="mt-4 w-full bg-white transform translate-y-0 shadow-sm rounded-md transition-shadow transition-transform duration-500 ease-in-out hover:-translate-y-1 hover:shadow-lg overflow-hidden cursor-pointer">
           <section>
             <div className="h-64 relative">
