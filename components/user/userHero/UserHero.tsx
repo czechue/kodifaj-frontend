8fa72e40bdcf82fa9d55e8add79a607dc2162ce2
jundymek <jundymek@gmail.com>
2020-05-11 21:46:04 +0200

    feat: UserHero buttons

--- components/user/userHero/UserHero.tsx
+++ components/user/userHero/UserHero.tsx
@@ -7,9 +7,9 @@ interface UserHeroProps {
 
 const UserHero: React.FC<UserHeroProps> = ({ login, photo }) => {
   return (
-    <div className="bg-white w-full">
-      <section className="max-w-8xl mx-auto">
-        <div className="flex flex-row ml-4 md:ml-8 py-8">
+    <div className="bg-white w-full pb-12 px-4">
+      <section className="max-w-8xl mx-auto flex flex-col md:flex-row justify-between md:items-center">
+        <div className="flex flex-row md:ml-8 py-8">
           <img
             src={photo}
             alt={`${login} photo`}
@@ -19,6 +19,12 @@ const UserHero: React.FC<UserHeroProps> = ({ login, photo }) => {
             <h1 className="text-2xl md:text-4xl font-bold">{login}</h1>
             <p className="text-xs md:text-sm text-gray-700 -mt-1">przykladowy.email@gmail.com</p>
           </div>
+        </div>
+        <div className="flex justify-center items-center h-full px-4 md:-mt-8">
+          <button className="bg-white border text-gray-700 h-10 w-32 md:w-40 rounded-md mr-8">
+            Button 1
+          </button>
+          <button className="bg-black text-white h-10 w-32 md:w-40 rounded-md">Button 2</button>
         </div>
       </section>
     </div>
