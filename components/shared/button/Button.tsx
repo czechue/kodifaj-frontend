06f826d511937449b6e124761ab1b9d9c741a60f
jundymek <jundymek@gmail.com>
2020-05-13 11:05:19 +0200

    style: change buttons color from black to gray-900

--- components/shared/button/Button.tsx
+++ components/shared/button/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ label, black }) => {
   const ButtonStyles = clsx(
     'hover:bg-gray-100 transition-colors border h-10 w-32 md:w-40 rounded-md mx-4 shadow-sm transform translate-y-0 transition-transform duration-500 hover:-translate-y-1',
-    black ? 'bg-black hover:bg-gray-800 text-white' : 'bg-white text-gray-700',
+    black ? 'bg-gray-900 hover:bg-gray-800 text-white' : 'bg-white text-gray-700',
   );
   return <button className={ButtonStyles}>{label}</button>;
 };
