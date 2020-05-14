46a1cc8635ef9a32307bf9ea380b2d3fe6734370
jundymek <jundymek@gmail.com>
2020-05-14 10:06:21 +0200

    refactor: change h1 to h2 in Hero

--- components/user/hero/Hero.tsx
+++ components/user/hero/Hero.tsx
@@ -18,7 +18,7 @@ const UserHero: React.FC<UserHeroProps> = ({ login, photo }) => {
             className=" w-16 h-16 md:w-32 md:h-32 rounded-full"
           />
           <div className="flex flex-col items-start ml-6 md:ml-12 -mt-2 md:mt-2">
-            <h1 className="text-2xl md:text-4xl font-bold">{login}</h1>
+            <h2 className="text-2xl md:text-4xl font-bold">{login}</h2>
             <p className="text-xs md:text-sm text-gray-700 -mt-2">przykladowy.email@gmail.com</p>
             <div className="flex mt-3 items-center">
               <FaGithub className="text-md mr-2" />
