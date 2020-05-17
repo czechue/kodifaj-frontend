8dba8724bf13cec5a9b6cc474210dbf32fad1840
jundymek <jundymek@gmail.com>
2020-05-17 23:30:09 +0200

    wip: Solutions

--- components/user/details/solutions/Solutions.tsx
+++ components/user/details/solutions/Solutions.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import Badges from '../../../badges/Badges';
 
-const UserSolutions: React.FC = () => {
-  return <section>Solutions</section>;
+interface UserSolutionsProps {
+  solutions?: string[];
+  login: string;
+  photo?: string;
+}
+
+const UserSolutions: React.FC<UserSolutionsProps> = ({ solutions, login, photo }) => {
+  console.log(solutions);
+  return (
+    <section className="w-full">
+      <h3>Rozwiązania zadań</h3>
+      <div className="w-full grid grid-cols-2 gap-4 mt-4">
+        {solutions?.map((solution) => {
+          return (
+            <section
+              key={solution}
+              className="w-full inline border border-gray-400 rounded-md flex flex-col px-4 py-4 relative bg-white"
+            >
+              <div className="flex items-center">
+                <img src={photo} alt={`${login} photo`} className="w-10 h-10 rounded-full mr-8" />
+                <h4>Karuzela w czystym JS</h4>
+              </div>
+              <p>Data dodania: 2019-10-21</p>
+              <div className="absolute right-0 top-0 mr-2">
+                <Badges tags={['html', 'css', 'js']} />
+              </div>
+            </section>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default UserSolutions;
