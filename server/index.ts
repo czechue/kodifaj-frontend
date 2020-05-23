126cc6e6988282bd457516e15cde41af3debb1ce
Michal Lester <michal.lester@allegro.pl>
2020-05-24 00:17:55 +0200

    empty server, remove old dependencies

--- server/index.ts
+++ server/index.ts
@@ -2,18 +2,18 @@ import next from 'next';
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import keys from './config/keys';
-import passport from 'passport';
+// import passport from 'passport';
 import bodyParser from 'body-parser';
-import cookieSession from 'cookie-session';
+// import cookieSession from 'cookie-session';
 
 // MODELS:
-import './models/User';
-import './models/Task';
-import './models/Solution';
+// import './models/User';
+// import './models/Task';
+// import './models/Solution';
 
 // import passportService from './services/passport';
-import tasksRoutes from './routes/tasks.routes';
-import endpoints from './endpoints';
+// import tasksRoutes from './routes/tasks.routes';
+// import endpoints from './endpoints';
 // import authRoutes from './routes/auth.routes';
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -29,18 +29,18 @@ app.prepare().then(() => {
   const server = express();
 
   server.use(bodyParser.json());
-  server.use(
-    cookieSession({
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      keys: [keys.cookieKey],
-    }),
-  );
-  server.use(passport.initialize());
-  server.use(passport.session());
+  // server.use(
+  //   cookieSession({
+  //     maxAge: 30 * 24 * 60 * 60 * 1000,
+  //     keys: [keys.cookieKey],
+  //   }),
+  // );
+  // server.use(passport.initialize());
+  // server.use(passport.session());
 
-  endpoints(server);
+  // endpoints(server);
   // authRoutes(app, server);
-  tasksRoutes(app, server);
+  // tasksRoutes(app, server);
 
   server.all('*', (req: Request, res: Response) => {
     return handle(req, res);
