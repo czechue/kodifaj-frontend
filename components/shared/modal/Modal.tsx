7d922b9f0f33376f0fadeb579cda5c3bb4f59fa8
jundymek <jundymek@gmail.com>
2020-06-08 23:40:04 +0200

    fix: blurred modal on desktop - remove transform property

--- components/shared/modal/Modal.tsx
+++ components/shared/modal/Modal.tsx
@@ -15,7 +15,7 @@ const Modal: React.FC<ModalProps> = ({ setIsOpen, title, children, isOpen }) => 
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none">
-        <section className="fixed w-11/12 md:w-8/12 lg:w-6/12 py-10 bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center shadow-lg">
+        <section className="fixed w-11/12 md:w-8/12 lg:w-6/12 py-10 bg-white flex flex-col justify-center items-center shadow-lg">
           <div className="w-full flex flex-col justify-center items-start px-8">
             <h2 className="text-3xl font-bold self-start">{title}</h2>
             {children}
