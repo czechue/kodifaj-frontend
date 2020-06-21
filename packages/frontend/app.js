3643e6fc7bec3163238bcd729f8197bb4efca003
michal.lester87@gmail.com <michal.lester87@gmail.com>
2020-06-21 23:40:44 +0200

    remove lintstaged and husky

--- packages/frontend/app.js
+++ packages/frontend/app.js
@@ -1,1 +1,22 @@
-require('./dist/index');
+// const cookieParser = require('cookie-parser');
+const express = require('express');
+const next = require('next');
+
+const app = next({ dev: false });
+
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    // server.use(cookieParser());
+
+    const port = process.env.PORT || '3000';
+    server.listen(port, () => console.log(`Server listening at localhost:${port}`));
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
